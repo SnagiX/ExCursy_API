ecbd98b30baa7f6c71771edebc45a06369945f3a
SnagiX <56646530+SnagiX@users.noreply.github.com>
2020-09-30 21:10:30 +0300

    comments of $_GET

--- api.php
+++ api.php
@@ -1,3 +1,30 @@
 <?php
+
+    /* 
+        $_GET attributes:
+        
+        Important:
+
+            +==========
+            type : text
+            +==========
+            What kind of query you responsed
+                
+                "marker_patt" : text
+                
+                Attributes for "marker_patt":
+
+                    +==========
+                    marker_id : int
+                    +==========
+                    Id of responsed marker
+
+
+        Not important:
+
+            -
+
+    */
+
     echo "Hello, world!";
 ?>
