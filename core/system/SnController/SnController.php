f6e4e54e616c0b55a0ed1ead5b09a1fd242f3eac
SnagiX <56646530+SnagiX@users.noreply.github.com>
2020-10-09 23:09:44 +0300

    Some changes:
    - pattern directory allowed for all users
    - New const - SN_DIRECTORY_WEBROOT
    - New const - SN_NETWORK_PROTOCOL
    - New attribute - pattern_link
    - Die condition for "show" functions
    - New method in SnController - file()
    - get_all_markers now returns pattern_link, without whole pattern

--- core/system/SnController/SnController.php
+++ core/system/SnController/SnController.php
@@ -27,6 +27,38 @@
         // PUBLIC FUNCTIONS
         // 
 
+        // Working with file:
+        // Attributes:
+        // $act : text ("read")     what do u need to do with file
+        // $file : text     link of file
+
+        public function file($act, $file) {
+
+            // read file:
+
+            function read($file) {
+
+                $handle = fopen($file, 'r');
+
+                $data = fread($handle, filesize($file));
+
+                fclose($handle);
+
+                return $data;
+            }
+            
+            switch ($act) {
+                case 'read':
+                    return read($file);
+                break;
+                
+                default:
+                    return read($file);
+                break;
+            }
+
+        }
+
     }
 
 ?>
